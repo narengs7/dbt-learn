db77bd5daa1e771215d37a733bd652276c5f655a
naren.gs@thedatateam.com <naren.gs@thedatateam.com>
2022-11-02 20:10:43 +0000

    sourcing

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,4 +1,3 @@
-with payments as (
     select 
         id as payment_id,
         orderid as order_id,
@@ -7,7 +6,5 @@ with payments as (
 
         amount / 100 as amount,
         created as created_at
-    from raw.stripe.payment
-)
+    from {{ source('stripe', 'payment') }}
 
-select * from payments
